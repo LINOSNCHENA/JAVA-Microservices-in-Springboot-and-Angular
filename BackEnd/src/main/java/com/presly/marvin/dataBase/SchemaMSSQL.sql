ae55047cae3fb24c6407e1edebf6924beab41fe7
LINOS NCHENA <linosnchena@gmail.com>
2019-10-12 15:02:44 +0200

    selection options

--- BackEnd/src/main/java/com/presly/marvin/dataBase/SchemaMSSQL.sql
+++ BackEnd/src/main/java/com/presly/marvin/dataBase/SchemaMSSQL.sql
@@ -14,5 +14,17 @@ CREATE TABLE dbo.bank1
    createdT DATETIME default GETDATE() not null
 );
 GO
--- Create the table in the specified schema
+
+GO -- Insert rows into table 'Clients' 
+INSERT INTO dbo.bank1
+   ([id],[names],[post],[dept],[salary],[status],[createdt])
+VALUES
+   (4, N'Wendy Nchena','Chelstone','x1',55,'dd','9/9/9'),
+   (3,N'Linos Presly','Pembahotmail','x2',55,'dd','6/6/6' ),
+   (2,N'Catherine Nikolas','MoscowYandex','x3',66,'xx','2/2/2'),
+   (1,N'Lorena Leon','Prahagmail','x4',66,'zm','4/4/4')
+GO
+SELECT * FROM dbo.bank1
+ORDER BY id DESC;
+GO -- Insert rows into table 'Transactions' 
 
--- BackEnd/src/main/java/com/presly/marvin/dataBase/SchemaPostGre.sql
+++ BackEnd/src/main/java/com/presly/marvin/dataBase/SchemaPostGre.sql
@@ -3,8 +3,7 @@ DROP TABLE IF EXISTS bank1 ;
 CREATE TABLE bank1 (
   id serial PRIMARY KEY,   name VARCHAR(90) NOT NULL,
   post VARCHAR(90), dept VARCHAR(90) default 'HUMAN-RESOURCE',
-  salary int default 62005,
-  status VARCHAR(90) default 'PART-TIME', 
+  salary int default 62005,  status VARCHAR(90) default 'PART-TIME', 
   createdt TIMESTAMP DEFAULT CURRENT_TIMESTAMP );
 
 INSERT INTO bank1 ( name, dept, post, salary )
